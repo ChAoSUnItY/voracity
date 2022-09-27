6b99faffec79092bd52816671cc7df8c42a85157
Kyle Lin <minecraft.kyle.train@gmail.com>
2022-09-28 00:53:02 +0800

    add ascii_whitespace

--- voracity/state.v
+++ voracity/state.v
@@ -4,9 +4,22 @@ import encoding.utf8 { raw_index, is_space }
 
 pub struct State {
 	input string
+	wd VoidParser
 mut:
 	pos int
 	cut int
 	error Error
-	wd VoidParser
+}
+
+pub fn ascii_whitespace(mut state State) {
+	for state.pos < state.input.len {
+		match state.input[state.pos] {
+			`\t`, `\n`, `\v`, `\f`, `\r`, ` ` {
+				state.pos++
+			}
+			else {
+				return
+			}
+		}
+	}
 }

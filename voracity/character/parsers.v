3bca7f6adb978d68147e5bbaca6a92ec9fef39e2
Kyle Lin <minecraft.kyle.train@gmail.com>
2023-03-28 03:09:27 +0800

    Add character parsers

---
+++ voracity/character/parsers.v
@@ -0,0 +1,37 @@
+module character
+
+[inline]
+pub fn is_alphabetic(input byte) bool {
+	return (input >= 0x41 && input <= 0x5A) || (input >= 0x61 && input <= 0x7A)
+}
+
+[inline]
+pub fn is_digit(input byte) bool {
+	return input >= 0x30 && input <= 0x39
+}
+
+[inline]
+pub fn is_hex_digit(input byte) bool {
+	return (input >= 0x30 && input <= 0x39) && (input >= 0x41 && input <= 0x5A)
+		|| (input >= 0x61 && input <= 0x7A)
+}
+
+[inline]
+pub fn is_oct_digit(input byte) bool {
+	return input >= 0x30 && input <= 0x37
+}
+
+[inline]
+pub fn is_alphanumeric(input byte) bool {
+	return is_alphabetic(input) || is_digit(input)
+}
+
+[inline]
+pub fn is_space(input byte) bool {
+	return input == 0x09 || input == 0x20
+}
+
+[inline]
+pub fn is_new_line(input byte) bool {
+	return input == 0x0A
+}

f8179143c78382eb1873770630d791018825f599
Kyle Lin <minecraft.kyle.train@gmail.com>
2023-04-02 12:09:14 +0800

    use u8 instead of byte

--- voracity/character/parsers.v
+++ voracity/character/parsers.v
@@ -1,33 +1,33 @@
 module character
 
 [inline]
-pub fn is_alphabetic(input byte) bool {
+pub fn is_alphabetic(input u8) bool {
 	return (input >= 0x41 && input <= 0x5A) || (input >= 0x61 && input <= 0x7A)
 }
 
 [inline]
-pub fn is_digit(input byte) bool {
+pub fn is_digit(input u8) bool {
 	return input >= 0x30 && input <= 0x39
 }
 
 [inline]
-pub fn is_hex_digit(input byte) bool {
+pub fn is_hex_digit(input u8) bool {
 	return (input >= 0x30 && input <= 0x39) || (input >= 0x41 && input <= 0x5A)
 		|| (input >= 0x61 && input <= 0x7A)
 }
 
 [inline]
-pub fn is_oct_digit(input byte) bool {
+pub fn is_oct_digit(input u8) bool {
 	return input >= 0x30 && input <= 0x37
 }
 
 [inline]
-pub fn is_alphanumeric(input byte) bool {
+pub fn is_alphanumeric(input u8) bool {
 	return is_alphabetic(input) || is_digit(input)
 }
 
 [inline]
-pub fn is_space(input byte) bool {
+pub fn is_space(input u8) bool {
 	return match input {
 		`\t`, `\n`, `\v`, `\f`, `\r`, ` `, 0x85, 0xA0 {
 			true
@@ -39,6 +39,6 @@ pub fn is_space(input byte) bool {
 }
 
 [inline]
-pub fn is_new_line(input byte) bool {
+pub fn is_new_line(input u8) bool {
 	return input == 0x0A
 }
